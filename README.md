0003513070900aa3da8ed85225324af52fa2d6f5
Soulou <leo@unbekandt.eu>
2015-04-24 16:30:05 +0200

    Init version

---
+++ README.md
@@ -0,0 +1,5 @@
+# Go netstat package
+
+This package is a wrapper around the data contained in /proc/net/dev. It
+returns structured data from this file.
+
---
+++ doc.go
@@ -0,0 +1,7 @@
+package netstat
+
+/*
+ * netstat package is a wrapper around the data contained
+ * in /proc/net/dev. It returns structured data from this
+ * file.
+ */
